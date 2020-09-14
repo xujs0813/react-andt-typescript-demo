729f60f83d7192e64eb8085474f5089a4701db71
xujs <907596538@qq.com>
2020-09-14 22:16:29 +0800

    feat: 引入 react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.scss';
 import {Button} from 'antd'
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Button type='dashed' size='middle'>Antd Button</Button>
+      <Router>
+        <Link to='/'>Home</Link>
+      </Router>
     </div>
   );
 }
